ad9264ed0b7d2c44715074dd08ef0590d55261bd
Álvaro <ainiguezdislaa@gmail.com>
2023-02-26 17:38:14 +0100

    ej terminado

--- src/dominio/ej14_4.java
+++ src/dominio/ej14_4.java
@@ -1,2 +1,9 @@
-package dominio;public class ej14_4 {
+package dominio;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+public class ej14_4 {
+    public static ArrayList listapares(int[]lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
